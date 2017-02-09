2126487eb2aa9d3dd271d01b12c63a4048adcbcd
Edison Ngo <edisonngo@gmail.com>
2017-02-08 18:11:56 -0800

    adds project file

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	listSpots();
+	function listSpots() {
+
+  $.getJSON("topspots.json", function(data){
+	$.each(data, function(index, item) {
+		var currentRow = "<tr><td>" + item.name + "</td><td>" + item.description + "</td><td><a href='https://www.google.com/maps?q=" + item.location + "'><button id='submitButton' class='btn btn - primary'>Open in Google Maps</button></a></td></tr>";
+    $(currentRow).appendTo('#goHere');
+
+})
+})
+};
+})
